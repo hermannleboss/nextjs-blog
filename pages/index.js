22128e30a98f62f23544b3d1442f779b42bfd94b
Hermann EHO <hermannleboss@gmail.com>
2022-07-16 17:00:51 +0000

    render post/[id]

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from "next/link";
-import {getSortedPostsData} from '../posts.js';
+import {getSortedPostsData} from "../lib/posts";
 
 export default function Home({allPostsData}) {
     return (
