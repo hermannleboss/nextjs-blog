9964efdf784cd4d856c151091615b363f25b2928
Hermann EHO <hermannleboss@gmail.com>
2022-07-16 14:09:11 +0000

    get statics props

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from "next/link";
-import { getSortedPostsData } from '../lib/posts';
-export default function Home({ allPostsData }) {
+import {getSortedPostsData} from '../posts.js';
+
+export default function Home({allPostsData}) {
     return (
         <Layout home>
             <Head>
@@ -23,22 +24,23 @@ export default function Home({ allPostsData }) {
                 </p>
             </section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <h2 className={utilStyles.headingLg}>Blog</h2>
-            <ul className={utilStyles.list}>
-                {allPostsData.map(({ id, date, title }) => (
-                    <li className={utilStyles.listItem} key={id}>
-                        {title}
-                        <br />
-                        {id}
-                        <br />
-                        {date}
-                    </li>
-                ))}
-            </ul>
-        </section>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({id, date, title}) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            {title}
+                            <br/>
+                            {id}
+                            <br/>
+                            {date}
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </Layout>
     );
 };
+
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
     return {
