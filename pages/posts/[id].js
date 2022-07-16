65f5085ee7421e157d58b75b09a01d3abca045e0
Hermann EHO <hermannleboss@gmail.com>
2022-07-16 17:06:43 +0000

    render markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,19 @@
 import Layout from '../../components/layout';
 import {getAllPostIds, getPostData} from "../../lib/posts";
-export default function Post({ postData }) {
+import Head from "next/head";
+
+export default function Post({postData}) {
     return (
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
             {postData.title}
             <br/>
             {postData.id}
             <br/>
             {postData.date}
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     );
 }
@@ -21,7 +27,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({params}) {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData,
